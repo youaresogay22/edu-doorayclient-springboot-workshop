9903a05be948898b6b565dcae17276e538a29a56
정지범 <zbum@naver.com>
2018-06-18 09:41:39 +0900

    solution

--- dooray-spring-boot-autoconfigure/src/main/java/com/nhnent/edu/springboot/starter/DoorayAutoConfiguration.java
+++ dooray-spring-boot-autoconfigure/src/main/java/com/nhnent/edu/springboot/starter/DoorayAutoConfiguration.java
@@ -1,6 +1,37 @@
 package com.nhnent.edu.springboot.starter;
 
+import com.nhnent.dooray.client.DoorayHookSender;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
+@ConditionalOnClass({DoorayHookSender.class})
+@ConditionalOnProperty(prefix = "dooray", name="hook-url")
+@EnableConfigurationProperties(DoorayProperties.class)
 public class DoorayAutoConfiguration {
+
+    @ConditionalOnMissingBean
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplateBuilder().build();
+    }
+
+    @Autowired
+    private DoorayProperties doorayProperties;
+
+    @Bean
+    public DoorayHookSender doorayHookSender(RestTemplate restTemplate) {
+        DoorayHookSender doorayHookSender = new DoorayHookSender(restTemplate, doorayProperties.getHookUrl());
+        return doorayHookSender;
+    }
 
 
 }
--- dooray-spring-boot-starter-application/src/main/java/com/nhnent/edu/springboot/starter/Application.java
+++ dooray-spring-boot-starter-application/src/main/java/com/nhnent/edu/springboot/starter/Application.java
@@ -1,4 +1,31 @@
 package com.nhnent.edu.springboot.starter;
 
+import com.nhnent.dooray.client.DoorayHook;
+import com.nhnent.dooray.client.DoorayHookSender;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
 public class Application {
+
+    @Autowired
+    private DoorayHookSender doorayHookSender;
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class);
+    }
+
+    @Bean
+    public ApplicationListener<ApplicationReadyEvent> applicationListener() {
+        return (applicationReadyEvent) -> {
+            DoorayHook doorayHook = new DoorayHook();
+            doorayHook.setBotName("정지범");
+            doorayHook.setText("테스트 중!");
+            doorayHookSender.send(doorayHook);
+        };
+    }
 }
